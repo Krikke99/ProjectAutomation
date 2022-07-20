f0ebbc6d60e0ba1eb6f212086bed1a952a079bd1
Chris Deprez <krikke99@gmail.com>
2022-07-20 17:06:54 +0200

    Parameter visibillity added

--- create.ps1
+++ create.ps1
@@ -1,6 +1,26 @@
+<#
+.PARAMETER fn
+Specifies the project name.
+
+.PARAMETER Visibillity
+Specifies the visibillity in Github. 1=private(default), 0=public.
+
+.EXAMPLE
+PS> create projectname
+
+.EXAMPLE
+PS> create projectname 0
+
+.LINK
+https://github.com/Krikke99
+
+#>
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$fn
+    [Parameter(Mandatory=$true, HelpMessage="Project name")]
+    [string]$fn,
+    
+    [Parameter(Mandatory=$false, HelpMessage="Set repository visibillity, 0=Public 1=Private")]
+    [int]$Visibillity = 1
 )
 
 $giturl="https://github.com/Krikke99/"
@@ -11,6 +31,12 @@ New-Item -Path $workingdir -ItemType Director
 Set-Location $workingdir
 New-Item "README.md" -ItemType File
 git init
+if (1 -eq $Visibillity) {
+    gh repo create $fn --private --source=. --remote=upstream
+}
+elseif (0 -eq $Visibillity) {
+    gh repo create $fn --public --source=. --remote=upstream
+}
 gh repo create $fn --private --source=. --remote=upstream
 git remote add origin $giturl$fn$gitext
 git add .
